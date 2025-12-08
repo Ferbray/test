7c912298938019a65c409a68ab60dab9f2d34c05
Ivan Gagaev <in.case.offers@gmail.com>
2025-12-08 19:40:24 +0300

    abc

--- Learn1/Learn1/obj/Debug/net9.0/Learn1.AssemblyInfo.cs
+++ Learn1/Learn1/obj/Debug/net9.0/Learn1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learn1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e010c629fca21b55e4c6902d1a1948343bd85110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f484852dedfdc0b67653de3dd9e9c55f955239e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learn1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learn1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
